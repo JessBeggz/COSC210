2d60bb0f8ab47b63df9de3801b877d53829406c3
Denton Manders <dentonmanders@gmail.com>
2026-01-29 18:30:17 -0800

    Added start and markComplete methods to workout + startApp to Main

--- src/test/WorkoutTest.java
+++ src/test/WorkoutTest.java
@@ -1,5 +1,11 @@
-package test;
-
-public class WorkoutTest {
-    
-}
+// package test;
+// import model.Workout;
+// import org.junit.Test;
+// import org.junit.jupiter.api.Assertions;
+// public class WorkoutTest {
+//     @Test
+//     public void nameCheck() {
+//         Workout workout = new Workout();
+//         assertTrue("True", workout.getName() == "Unnamed Workout");
+//     }
+// }
