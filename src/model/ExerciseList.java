3ee698232bab546d36059bdadeba1344b91122a3
JessBeggz <jessie.beggz@gmail.com>
2026-01-29 19:03:11 -0800

    Added clauses to methods in ExerciseList class

--- src/model/ExerciseList.java
+++ src/model/ExerciseList.java
@@ -8,18 +8,29 @@ public class ExerciseList {
         list = new ArrayList<>();
     }
 
+    // REQUIRES: e cannot be null
+    // MODIFIES: this
+    // EFFECTS: If the list does not already contain the exercise that is wanted to be added, then add the exercise to the list
     public void add(Exercise e) {
-        list.add(e);
+        if (!list.contains(e))
+            list.add(e);
     }
 
+    // REQUIRES: e cannot be null
+    // MODIFIES: this
+    // EFFECTS: If the list contains the exercise that is wanted to be removed, then remove the exercise from the list
     public void remove(Exercise e) {
-        list.remove(e);
+        if (list.contains(e))
+            list.remove(e);
     }
 
+    // REQUIRES: e cannot be null
+    // EFFECTS: returns a boolean. true if list contains the exercise and false if the list does not contain the exercise
     public boolean contains(Exercise e) {
         return list.contains(e);
     }
 
+    // EFFECTS: returns an int representing the size of the list
     public int size(){
         return list.size();
     }
