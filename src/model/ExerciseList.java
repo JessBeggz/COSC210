62317f3f811fc9d2542568cfc4eadd0ead46bf11
JessBeggz <jessie.beggz@gmail.com>
2026-01-29 20:15:33 -0800

    Fixed add method in ExerciseList to not allow duplicate Exercises

--- src/model/ExerciseList.java
+++ src/model/ExerciseList.java
@@ -3,17 +3,36 @@ import java.util.*;
 //This class represents an Exercise List
 public class ExerciseList {
     private ArrayList<Exercise> list;
+    private Exercise squat = new Exercise("Squat");
+    private Exercise hipThrust = new Exercise("Hip Thrust");
+    private Exercise benchPress = new Exercise("Bench Press");
+    private Exercise legPress = new Exercise("Leg Press");
+    private Exercise bicepCurls = new Exercise("Bicep Curls");
+    private Exercise legCurls = new Exercise("Leg Curls");
+    private Exercise latPulldown = new Exercise("Lat Pulldown");
 
     public ExerciseList() {
         list = new ArrayList<>();
+        list.add(squat);
+        list.add(hipThrust);
+        list.add(benchPress);
+        list.add(legPress);
+        list.add(bicepCurls);
+        list.add(legCurls);
+        list.add(latPulldown);
     }
 
     // REQUIRES: e cannot be null
     // MODIFIES: this
-    // EFFECTS: If the list does not already contain the exercise that is wanted to be added, then add the exercise to the list
+    // EFFECTS: If the list does not already contain the exercise name that is wanted to be added, then add the exercise to the list
     public void add(Exercise e) {
-        if (!list.contains(e))
-            list.add(e);
+        int count = 0;
+        for (int i = 0; i < list.size(); i++) {
+            if (!e.getName().equals(list.get(i).getName()))
+                count++;
+            if (count==list.size())
+                list.add(e);
+        }
     }
 
     // REQUIRES: e cannot be null
@@ -33,5 +52,12 @@ public class ExerciseList {
     // EFFECTS: returns an int representing the size of the list
     public int size(){
         return list.size();
+    }
+
+    // EFFECTS: Prints the names of the exercises in the list
+    public void view(){
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i).getName());
+        }
     }
 }
--- src/test/ExerciseListTest.java
+++ src/test/ExerciseListTest.java
@@ -14,15 +14,17 @@ public class ExerciseListTest {
     @BeforeEach
     public void setUp() {
         list = new ExerciseList();
-        a = new Exercise("pullUp");
-        b = new Exercise("pushUp");
+        a = new Exercise("Squat");
+        b = new Exercise("Jump");
     }
 
     @Test
     public void testAdd() {
-        assertEquals(0, list.size());
+        assertEquals(7, list.size());
         list.add(a);
-        assertEquals(1, list.size());
+        assertEquals(7, list.size()); //Should stay the same (7) cause an exercise with the name Squat already exists in the list
+        list.add(b);
+        assertEquals(8, list.size()); //Should change to 8 cause no exercise with the name Jump already exists in the list
         
     }
 
@@ -41,4 +43,11 @@ public class ExerciseListTest {
         list.remove(a);
         assertFalse(list.contains(a));
     }
+
+    @Test
+    public void testView() {
+        list.add(a);
+        list.view();
+    }
+
 }
