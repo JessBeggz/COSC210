82ac85a79a0805e839d9e4b20009ac379752c77f
JessBeggz <jessie.beggz@gmail.com>
2026-02-01 00:22:45 -0800

    Added user story: Choosing an exercise from a list to add to a workout.

--- src/model/Main.java
+++ src/model/Main.java
@@ -1,5 +1,4 @@
 package model;
-
 import java.util.Scanner;
 
 public class Main {
@@ -8,26 +7,23 @@ public class Main {
         ExerciseList exerciseList = new ExerciseList();
         startApp(workoutList, exerciseList);
     }
-
+    
     public static void startApp(WorkoutList workoutList, ExerciseList exerciseList) {
         Scanner scanner = new Scanner(System.in);
         boolean homePageRun = true;
         while(homePageRun) {
             System.out.println("Type '1' to begin new workout");
             System.out.println("Type '2' to view workout history");
-            System.out.println("Type '3' to create an exercise");
+            System.out.println("Type '3' to view Exercise List");
+            System.out.println("Type '4' to create an exercise");
             int userSelection = scanner.nextInt();
 
         switch(userSelection) {
             case 1:Workout workout = new Workout();
-                 workout.createWorkout(workout, workoutList, exerciseList);
-                 break;
-            case 2:
-            workoutList.viewWorkoutList();
-            break;
-            case 3:
-                 exerciseList.createExercise();
-                 break;
+                workout.createWorkout(workout, workoutList, exerciseList); break;
+            case 2: workoutList.viewWorkoutList(); break;
+            case 3: exerciseList.view(); break;
+            case 4: exerciseList.createExercise(); break;
         }
         }
         scanner.close();
