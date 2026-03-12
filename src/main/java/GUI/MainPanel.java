b50526841e92c990c5c130f1a0e4ac1c5cd0dd4a
JessBeggz <jessie.beggz@gmail.com>
2026-03-12 16:20:42 -0700

    added reload panel

--- src/main/java/GUI/MainPanel.java
+++ src/main/java/GUI/MainPanel.java
@@ -1,6 +1,7 @@
 package GUI;
 
 import java.awt.Color;
+import java.io.File;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -9,22 +10,35 @@ public class MainPanel extends JPanel{
     
     HomePanel buttonPanel;
     ExitPanel exitPanel;
+    ReloadPanel reloadPanel;
+    File exerciseListData;
+    File workoutListData;
+
     public MainPanel(JFrame frame) {
+        exerciseListData = new File("data/exerciseListData.json");
+        workoutListData = new File("data/workoutListData.json");
         exitPanel = new ExitPanel(frame, this);
         buttonPanel = new HomePanel(frame, this);
+        reloadPanel = new ReloadPanel(frame, this);
 
         setBackground(Color.RED);
-        add(buttonPanel);
-        setVisible(true);
+
+        if(!exerciseListData.exists() && !workoutListData.exists()) {
+            add(buttonPanel);
+            setVisible(true);
+        } else {
+            add(reloadPanel);
+            setVisible(true);
+        }
     }
 
     public void showExitPanel() {
-                add(exitPanel);
-
+        add(exitPanel);
         exitPanel.setVisible(true);
     }
 
     public void showButtonPanel() {
+        add(buttonPanel);
         buttonPanel.setVisible(true);
     }
 }
